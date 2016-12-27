543fd12a7f70bdd3f5d085c3d97c80a11986d242
tonyz840101 <tonyz840101@gmail.com>
2016-12-28 01:52:18 +0800

    Add files via upload

--- level_data.js
+++ level_data.js
@@ -1,17 +1,18 @@
 function load_level(){
 	switch(level){
 		case 0:
-			enemies[enemies.length] = {x: 300, y: 25, w: 20, dx: -3, dy: 0, t: 0, speed: 4, color: "#22ff22",
+			//spawn_buff(true,2);
+			enemies[enemies.length] = {x: 300, y: 25, w: 20, dx: -3, dy: 0, t: 0, speed: 2, color: "#22ff22",
 				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 500, y_lower_limit: 0};
-			enemies[enemies.length] = {x: 100, y: 25, w: 20, dx: 3, dy: 0, t: 0, speed: 4, color: "#22ff22",
+			enemies[enemies.length] = {x: 100, y: 25, w: 20, dx: 3, dy: 0, t: 0, speed: 2, color: "#22ff22",
 				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 500, y_lower_limit: 0};
-			enemies[enemies.length] = {x: 275, y: 50, w: 20, dx: -2, dy: 0, t: 0, speed: 5, color: "#22ff22",
+			enemies[enemies.length] = {x: 275, y: 50, w: 20, dx: -2, dy: 0, t: 0, speed: 3, color: "#22ff22",
 				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 500, y_lower_limit: 0};
-			enemies[enemies.length] = {x: 125, y: 50, w: 20, dx: 2, dy: 0, t: 0, speed: 5, color: "#22ff22",
+			enemies[enemies.length] = {x: 125, y: 50, w: 20, dx: 2, dy: 0, t: 0, speed: 3, color: "#22ff22",
 				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 500, y_lower_limit: 0};
-			enemies[enemies.length] = {x: 250, y: 75, w: 20, dx: -1, dy: 0, t: 0, speed: 6, color: "#22ff22",
+			enemies[enemies.length] = {x: 250, y: 75, w: 20, dx: -1, dy: 0, t: 0, speed: 4, color: "#22ff22",
 				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 500, y_lower_limit: 0};
-			enemies[enemies.length] = {x: 150, y: 75, w: 20, dx: 1, dy: 0, t: 0, speed: 6, color: "#22ff22",
+			enemies[enemies.length] = {x: 150, y: 75, w: 20, dx: 1, dy: 0, t: 0, speed: 4, color: "#22ff22",
 				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 500, y_lower_limit: 0};
 			level_text = "1-2";
 			break
@@ -31,7 +32,6 @@ function load_level(){
 			level_text = "1-3";
 			break
 		case 2:
-			spawn_buff(true);
 			enemies[enemies.length] = {x: 300, y: 50, w: 20, dx: -3, dy: 1, t: 0, speed: 4, color: "#ff2222",
 				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 500, y_lower_limit: 0};
 			enemies[enemies.length] = {x: 100, y: 50, w: 20, dx: 3, dy: 1, t: 0, speed: 4, color: "#ff2222",
@@ -51,7 +51,7 @@ function load_level(){
 			level_text = "1-4";
 			break
 		case 3:
-			spawn_buff(true);
+			spawn_buff(true,1);
 			enemies[enemies.length] = {x: 300, y: 25, w: 20, dx: -3, dy: 1, t: 0, speed: 4, color: "#22ff22",
 				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 200, y_lower_limit: 0};
 			enemies[enemies.length] = {x: 100, y: 25, w: 20, dx: 3, dy: 1, t: 0, speed: 4, color: "#22ff22",
@@ -68,10 +68,10 @@ function load_level(){
 			break;
 		case 4:
 			for(i=1; i<40; i+=2.5){
-				Bullets[1][Bullets[1].length] = { x: i*10, y: -150, d: 4 ,c: "#ff2222"};
-				Bullets[1][Bullets[1].length] = { x: i*10, y: -100, d: 4 ,c: "#ff2222"};
-				Bullets[1][Bullets[1].length] = { x: i*10, y: -50, d: 4 ,c: "#ff2222"};
-				Bullets[1][Bullets[1].length] = { x: i*10, y: 0, d: 4 ,c: "#ff2222"};
+				Bullets[1][Bullets[1].length] = { x: i*10, y: -190, d: 4 ,c: "#ff2222"};
+				Bullets[1][Bullets[1].length] = { x: i*10+5, y: -100, d: 4 ,c: "#ff2222"};
+				Bullets[1][Bullets[1].length] = { x: i*10, y: -10, d: 4 ,c: "#ff2222"};
+				Bullets[1][Bullets[1].length] = { x: i*10+5, y: 80, d: 4 ,c: "#ff2222"};
 			}
 			enemies[enemies.length] = {x: 380, y: 20, w: 12, dx: -7, dy: 5, t: 3, speed: 2, color: "#9955FF",
 				x_upper_limit: 400, x_lower_limit: 300, y_upper_limit: 700, y_lower_limit: 0};
@@ -88,8 +88,9 @@ function load_level(){
 			level_text = "Final";
 			break;
 		case 5:
-			enemies[enemies.length] = {x: 200, y: 50, w: 10, dx: 1, dy: 0, t: 2, speed: 2, color: "#5555FF", blood: 20,
-				x_upper_limit: 350, x_lower_limit: 50, y_upper_limit: 500, y_lower_limit: 0, inv: false};
+			enemies[enemies.length] = {x: 200, y: 50, w: 10, dx: 1, dy: 0, t: 2, speed: 2, color: "#FF3333", blood: 20,
+				x_upper_limit: 350, x_lower_limit: 50, y_upper_limit: 500, y_lower_limit: 0, inv: false};//5555ff
+				level_text = "Ready";
 			break;
 		default:
 			break;
