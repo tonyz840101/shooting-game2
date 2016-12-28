98f3726842f91ba442bf407b4724f1b7f24f2c7b
tonyz840101 <tonyz840101@gmail.com>
2016-12-29 03:16:14 +0800

    beta version
    level 1-1 to 1-5 and final stage complete
    after boss stage, there are levels random-created by function
    effects like bullet-hit effect, life-change effect are added
    planes' color adjusted
    game balance improved

--- level_data.js
+++ level_data.js
@@ -1,19 +1,139 @@
 function load_level(){
 	switch(level){
 		case 0:
-			spawn_buff(true,2);
-			enemies[enemies.length] = {x: 300, y: 25, w: 20, dx: -3, dy: 0, t: 0, speed: 2, color: "#22ff22",
-				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 500, y_lower_limit: 0};
-			enemies[enemies.length] = {x: 100, y: 25, w: 20, dx: 3, dy: 0, t: 0, speed: 2, color: "#22ff22",
-				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 500, y_lower_limit: 0};
-			enemies[enemies.length] = {x: 275, y: 50, w: 20, dx: -2, dy: 0, t: 0, speed: 3, color: "#22ff22",
-				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 500, y_lower_limit: 0};
-			enemies[enemies.length] = {x: 125, y: 50, w: 20, dx: 2, dy: 0, t: 0, speed: 3, color: "#22ff22",
-				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 500, y_lower_limit: 0};
-			enemies[enemies.length] = {x: 250, y: 75, w: 20, dx: -1, dy: 0, t: 0, speed: 4, color: "#22ff22",
-				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 500, y_lower_limit: 0};
-			enemies[enemies.length] = {x: 150, y: 75, w: 20, dx: 1, dy: 0, t: 0, speed: 4, color: "#22ff22",
-				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 500, y_lower_limit: 0};
+			//spawn_buff(true,2);
+			enemies[enemies.length] = {x: 300, y: 25, w: 20, dx: -3, dy: 1, t: 0, speed: 2, color: "#22ff22",
+				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 300, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 100, y: 25, w: 20, dx: 3, dy: 1, t: 0, speed: 2, color: "#22ff22",
+				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 300, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 275, y: 50, w: 20, dx: -2, dy: 1, t: 0, speed: 3, color: "#22ff22",
+				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 300, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 125, y: 50, w: 20, dx: 2, dy: 1, t: 0, speed: 3, color: "#22ff22",
+				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 300, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 250, y: 75, w: 20, dx: -1, dy: 1, t: 0, speed: 4, color: "#22ff22",
+				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 300, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 150, y: 75, w: 20, dx: 1, dy: 1, t: 0, speed: 4, color: "#22ff22",
+				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 300, y_lower_limit: 0, bullet: 1};
+			level_text = "1-2";
+			break
+		case 1:
+			enemies[enemies.length] = {x: 280, y: 25, w: 20, dx: -3, dy: 1, t: 0, speed: 2, color: "#22ff22",
+				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 300, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 120, y: 25, w: 20, dx: 3, dy: 1, t: 0, speed: 2, color: "#22ff22",
+				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 300, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 300, y: 20, w: 20, dx: 0, dy: 3, t: 0, speed: 3, color: "#22ff22",
+				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 300, y_lower_limit: -50, bullet: 2};
+			enemies[enemies.length] = {x: 100, y: 20, w: 20, dx: 0, dy: 3, t: 0, speed: 3, color: "#22ff22",
+				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 300, y_lower_limit: -50, bullet: 2};
+			enemies[enemies.length] = {x: 250, y: 75, w: 20, dx: -1, dy: 1, t: 0, speed: 4, color: "#22ff22",
+				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 300, y_lower_limit: 0, bullet: 3};
+			enemies[enemies.length] = {x: 150, y: 75, w: 20, dx: 1, dy: 1, t: 0, speed: 4, color: "#22ff22",
+				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 300, y_lower_limit: 0, bullet: 3};
+			level_text = "1-3";
+			break
+		case 2:
+			enemies[enemies.length] = {x: 250, y: 30, w: 20, dx: 1, dy: 2, t: 3, speed: 3, color: "#22ff22", blood: 1,
+				x_upper_limit: 500, x_lower_limit: -100, y_upper_limit: 550, y_lower_limit: -50, bullet: 2};
+			enemies[enemies.length] = {x: 150, y: 30, w: 20, dx: -1, dy: 2, t: 3, speed: 3, color: "#22ff22", blood: 1,
+				x_upper_limit: 500, x_lower_limit: -100, y_upper_limit: 550, y_lower_limit: -50, bullet: 2};
+			level_text = "1-4";
+			break
+		case 3:
+			enemies[enemies.length] = {x: 300, y: 25, w: 20, dx: 1, dy: 1, t: 0, speed: 2, color: "#22ff22",
+				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 200, y_lower_limit: 0, bullet: 3};
+			enemies[enemies.length] = {x: 100, y: 25, w: 20, dx: -1, dy: 1, t: 0, speed: 2, color: "#22ff22",
+				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 200, y_lower_limit: 0, bullet: 3};
+			enemies[enemies.length] = {x: 325, y: 25, w: 16, dx: -3, dy: 2, t: 3, speed: 3, color: "#22ff22", blood: 1,
+				x_upper_limit: 400, x_lower_limit: 250, y_upper_limit: 600, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 75, y: 25, w: 16, dx: 3, dy:2, t: 3, speed: 3, color: "#22ff22", blood: 1,
+				x_upper_limit: 150, x_lower_limit: 0, y_upper_limit: 600, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 325, y: 25, w: 16, dx: 3, dy: 2, t: 3, speed: 2, color: "#22ff22", blood: 1,
+				x_upper_limit: 400, x_lower_limit: 250, y_upper_limit: 600, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 75, y: 25, w: 16, dx: -3, dy: 2, t: 3, speed: 2, color: "#22ff22", blood: 1,
+				x_upper_limit: 150, x_lower_limit: 0, y_upper_limit: 600, y_lower_limit: 0, bullet: 1};
+			level_text = "1-5";
+			break;
+		case 4:
+			spawn_buff(true,1);
+			enemies[enemies.length] = {x: 380, y: 20, w: 12, dx: -1, dy: 1, t: 3, speed: 2, color: "#22ff22", blood: 1,
+				x_upper_limit: 400, x_lower_limit: 0, y_upper_limit: 400, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 20, y: 20, w: 12, dx: 1, dy: 1, t: 3, speed: 2, color: "#22ff22", blood: 1,
+				x_upper_limit: 400, x_lower_limit: 0, y_upper_limit: 400, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 380, y: 580, w: 12, dx: -1, dy: -1, t: 3, speed: 2, color: "#22ff22", blood: 1,
+				x_upper_limit: 400, x_lower_limit: 0, y_upper_limit: 600, y_lower_limit: 200, bullet: 1};
+			enemies[enemies.length] = {x: 20, y: 580, w: 12, dx: 1, dy: -1, t: 3, speed: 2, color: "#22ff22", blood: 1,
+				x_upper_limit: 400, x_lower_limit: 0, y_upper_limit: 600, y_lower_limit: 200, bullet: 1};
+			enemies[enemies.length] = {x: 380, y: 20, w: 12, dx: 0, dy: 1, t: 3, speed: 4, color: "#22ff22", blood: 1,
+				x_upper_limit: 400, x_lower_limit: 0, y_upper_limit: 650, y_lower_limit: -50, bullet: 2};
+			enemies[enemies.length] = {x: 20, y: 20, w: 12, dx: 0, dy: 1, t: 3, speed: 4, color: "#22ff22", blood: 1,
+				x_upper_limit: 400, x_lower_limit: 0, y_upper_limit: 650, y_lower_limit: -50, bullet: 2};
+			enemies[enemies.length] = {x: 20, y: 50, w: 24, dx: 2, dy: 0, t: 3, speed: 4, color: "#00FF99", blood: 10,
+				x_upper_limit: 400, x_lower_limit: 0, y_upper_limit: 650, y_lower_limit: -50, bullet: 2};
+			level_text = "Final";
+			break;
+		case 5:
+			between_level_refresh_status();
+			enemies[enemies.length] = {x: 200, y: 50, w: 10, dx: 1, dy: 0, t: 2, speed: 2, color: "#FF3333", blood: 20,
+				x_upper_limit: 350, x_lower_limit: 50, y_upper_limit: 500, y_lower_limit: 0, inv: false};//5555ff
+				level_text = "Ready";
+			break;
+		default:
+			var type = Math.floor(Math.random() * 20)%4;
+			var x_speed = Math.floor(Math.random() * 20)%5;
+			var y_speed = Math.floor(Math.random() * 20)%5;
+				switch(type){
+					case 0:
+						for(i = 3; i<400; i+=40){
+						var bullet_type = Math.floor(Math.random() * 20)%3;
+						enemies[enemies.length] = {x: i, y: 25, w: 12, dx: (-1*x_speed), dy: y_speed, t: 3, speed: 3, color: "#ff0000", blood: 1,
+							x_upper_limit: 450, x_lower_limit: -50, y_upper_limit: 550, y_lower_limit: 0, bullet: bullet_type+1};
+						enemies[enemies.length] = {x: i+5, y: 40, w: 12, dx: x_speed, dy: -1*y_speed, t: 3, speed: 3, color: "#ff0000", blood: 1,
+							x_upper_limit: 450, x_lower_limit: -50, y_upper_limit: 550, y_lower_limit: 0, bullet: bullet_type+1};
+						}
+						break;
+					case 1:
+						for(i = 3; i<410; i+=40){
+						var bullet_type = Math.floor(Math.random() * 20)%2;
+						enemies[enemies.length] = {x: i, y: 25, w: 20, dx: (-1*x_speed), dy: y_speed, t: 0, speed: 3, color: "#ff0000",
+							x_upper_limit: 450, x_lower_limit: -50, y_upper_limit: 550, y_lower_limit: 0, bullet: bullet_type+1};
+						enemies[enemies.length] = {x: i+5, y: 40, w: 20, dx: x_speed, dy: -1*y_speed, t: 0, speed: 3, color: "#ff0000",
+							x_upper_limit: 450, x_lower_limit: -50, y_upper_limit: 550, y_lower_limit: 0, bullet: bullet_type+1};
+						}
+						break;
+					case 2:
+						for(i = 3; i<400; i+=40){
+						enemies[enemies.length] = {x: i, y: 25, w: 12, dx: (-1*x_speed), dy: y_speed, t: 1, speed: 3, color: "#ff0000", blood: 1,
+							x_upper_limit: 450, x_lower_limit: -50, y_upper_limit: 550, y_lower_limit: 0};
+						enemies[enemies.length] = {x: i+5, y: 40, w: 12, dx: x_speed, dy: -1*y_speed, t: 1, speed: 3, color: "#ff0000", blood: 1,
+							x_upper_limit: 450, x_lower_limit: -50, y_upper_limit: 550, y_lower_limit: 0};
+						}
+						break;
+					default:
+						enemies[enemies.length] = {x: 200, y: 50, w: 10, dx: 1, dy: 0, t: 2, speed: 2, color: "#FF3333", blood: 20,
+							x_upper_limit: 350, x_lower_limit: 50, y_upper_limit: 500, y_lower_limit: 0, inv: false};//5555ff
+						break;
+			}
+			break;
+	}
+	level ++;
+}
+/*
+function load_level(){
+	switch(level){
+		case 0:
+			//spawn_buff(true,2);
+			enemies[enemies.length] = {x: 300, y: 25, w: 20, dx: -3, dy: 1, t: 0, speed: 2, color: "#22ff22",
+				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 500, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 100, y: 25, w: 20, dx: 3, dy: 1, t: 0, speed: 2, color: "#22ff22",
+				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 500, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 275, y: 50, w: 20, dx: -2, dy: 1, t: 0, speed: 3, color: "#22ff22",
+				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 500, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 125, y: 50, w: 20, dx: 2, dy: 1, t: 0, speed: 3, color: "#22ff22",
+				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 500, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 250, y: 75, w: 20, dx: -1, dy: 1, t: 0, speed: 4, color: "#22ff22",
+				x_upper_limit: 400, x_lower_limit: 150, y_upper_limit: 500, y_lower_limit: 0, bullet: 1};
+			enemies[enemies.length] = {x: 150, y: 75, w: 20, dx: 1, dy: 1, t: 0, speed: 4, color: "#22ff22",
+				x_upper_limit: 250, x_lower_limit: 0, y_upper_limit: 500, y_lower_limit: 0, bullet: 1};
 			level_text = "1-2";
 			break
 		case 1:
@@ -96,38 +216,5 @@ function load_level(){
 			break;
 	}
 	level ++;
-/*
-enemies[enemies.length] = {x: 300, y: 50, w: 10, dx: -2, dy: 3, t: 1, speed: 1, color: "#00FFFF"};
-enemies[enemies.length] = {x: 100, y: 50, w: 10, dx: 2, dy: 3, t: 1, speed: 1, color: "#00FFFF"};
-enemies[enemies.length] = {x: 300, y: 100, w: 20, dx: -1, dy: 0, t: 0, speed: 2, color: "#ff2222"};
-enemies[enemies.length] = {x: 100, y: 100, w: 20, dx: 1, dy: 0, t: 0, speed: 2, color: "#ff2222"};
-enemies[enemies.length] = {x: 300, y: 50, w: 10, dx: -2, dy: 3, t: 1, speed: 1, color: "#ffffff"};
-enemies[enemies.length] = {x: 100, y: 50, w: 10, dx: 2, dy: 3, t: 1, speed: 1, color: "#ffffff"};
-enemies[enemies.length] = {x: 300, y: 100, w: 20, dx: -1, dy: 0, t: 0, speed: 2, color: "#ffffff"};
-enemies[enemies.length] = {x: 100, y: 100, w: 20, dx: 1, dy: 0, t: 0, speed: 2, color: "#ff2222"};*/
 }
-/*
-function level2(){
-enemies[enemies.length] = {x: 300, y: 50, w: 10, dx: -2, dy: 3, t: 1, speed: 1, color: "#2222ff"};
-enemies[enemies.length] = {x: 100, y: 50, w: 10, dx: 2, dy: 3, t: 1, speed: 1, color: "#2222ff"};
-enemies[enemies.length] = {x: 300, y: 100, w: 20, dx: -1, dy: 0, t: 0, speed: 2, color: "#ff2222"};
-enemies[enemies.length] = {x: 100, y: 100, w: 20, dx: 1, dy: 0, t: 0, speed: 2, color: "#ff2222"};
-
-}
-
-function level3(){
-enemies[enemies.length] = {x: 380, y: 100, w: 20, dx: -1, dy: 0, t: 0, speed: 2, color: "#ff2222"};
-enemies[enemies.length] = {x: 20, y: 100, w: 20, dx: 1, dy: 0, t: 0, speed: 2, color: "#ff2222"};
-enemies[enemies.length] = {x: 300, y: 70, w: 20, dx: -5, dy: 0, t: 0, speed: 1, color: "#22ff22"};
-enemies[enemies.length] = {x: 100, y: 70, w: 20, dx: 5, dy: 0, t: 0, speed: 1, color: "#22ff22"};
-}
-
-function level4(){
-enemies[enemies.length] = {x: 300, y: 50, w: 10, dx: -2, dy: 3, t: 1, speed: 1, color: "#2222ff"};
-enemies[enemies.length] = {x: 100, y: 50, w: 10, dx: 2, dy: 3, t: 1, speed: 1, color: "#2222ff"};
-enemies[enemies.length] = {x: 300, y: 100, w: 20, dx: -1, dy: 0, t: 0, speed: 2, color: "#ff2222"};
-enemies[enemies.length] = {x: 100, y: 100, w: 20, dx: 1, dy: 0, t: 0, speed: 2, color: "#ff2222"};
-}
-
-function boss(){
-}*/
+*/
